89027a6307b0f82074127b2d0a31f0ac673b4421
heloabrantessb <heloisa.abrantesdev@gmail.com>
2026-04-18 17:18:52 -0300

    refactor: integra UsersController ao UserService e centraliza regras de negócio

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,7 +1,7 @@
 const knex = require('../database/knex');
 const bcrypt = require('bcryptjs');
 const AppError = require('../utils/AppError');
-const userService = require('../services/UserService');
+const {createUser, listUsers, findUserById, findUserByEmail, deleteUser} = require('../services/UserService');
 
 class UsersController {
     async create(req, res) {
@@ -11,44 +11,48 @@ class UsersController {
             return res.status(400).json({ error: 'Preencha todos os campos' });
         }
 
-        const userExists = await knex('users').where({email}).first();
+        const userExists = await findUserByEmail(email);
         if (userExists) {
             throw new AppError('Usuário já cadastrado');
         }
 
         const hashedPassword = await bcrypt.hash(password, 8);
 
-        await userService.createUser(name, email, hashedPassword);
+        await createUser(name, email, hashedPassword);
 
         return res.status(201).json('Usuário criado com sucesso');
     }
 
-    async listUsers(req, res) {
-        const users = await knex('users');
+    async list(req, res) {
+        const users = await listUsers();
         return res.status(200).json(users);
     }
 
-    async listUsersById(req, res) {
+    async findById(req, res) {
         const {id} = req.params
-        const users = await knex('users').where({id})
+        const users = await findUserById(id)
         return res.status(200).json(users)
     }
 
-    async deleteUser(req, res) {
+    async findByEmail(req, res) {
+        const {email} = req.params
+        const users = await findUserByEmail(email)
+        return res.status(200).json(users)
+    }
+
+    async delete(req, res) {
         const { id } = req.params;
-        await knex('users').where({id}).delete();
+        await deleteUser(id);
 
         return res.status(200).json('Ususário deletado com sucesso');
     }
 
-    async updateUser(req, res) {
+    async update(req, res) {
         const { name, email, password } = req.body;
         const { id } = req.user;
 
-        console.log(id);
-
         if (email) {
-            const userExists = await knex('users').where({email}).first();
+            const userExists = await findUserByEmail(email);
             if (userExists) {
                 throw new AppError('Usuário já cadastrado');
             }
@@ -57,17 +61,11 @@ class UsersController {
         if (password) {
             const hashedPassword = await bcrypt.hash(password, 8);
 
-            await knex('users').where({id}).update({
-                name,
-                email,
-                password: hashedPassword,
-            })
+            await this.updateUser(id, name, email, hashedPassword)
+
             return res.status(200).json('Usuário atualizado com sucesso');
         } else {
-            await knex('users').where({id}).update({
-                name,
-                email
-            })
+            await this.updateUser(id, name, email)
             return res.status(200).json('Usuário atualizado com sucesso');
         }
     }
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -12,6 +12,52 @@ const createUser = async (name, email, password) => {
     }
 }
 
+const listUsers = async () => {
+    const users = await knex("users")
+    return users;
+}
+
+const findUserById = async (id) => {
+    const user = await knex("users").where({ id }).first()
+    return user;
+}
+
+const findUserByEmail = async (email) => {
+    const user = await knex("users").where({ email }).first()
+    return user;
+}
+
+const updateUser = async (id, name, email, password) => {
+    const user = await knex("users").where({ id }).first()
+
+    if (!user) {
+        throw new AppError('Usuário não encontrado');
+    }
+
+    if (email) user.email = email;
+    if (name) user.name = name;
+    if (password) user.password = password;
+
+    await knex("users").where({ id }).update(user)
+    return user;
+}
+
+const deleteUser = async (id) => {
+    const user = await knex("users").where({ id }).first()
+    
+    if (!user) {
+        throw new AppError('Usuário não encontrado');
+    }
+
+    await knex("users").where({ id }).delete()
+    return;
+}
+
 module.exports = {
     createUser,
+    listUsers,
+    findUserById,
+    findUserByEmail,
+    updateUser,
+    deleteUser
 }
